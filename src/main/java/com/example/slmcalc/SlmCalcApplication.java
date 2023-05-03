b3975f29ca918930adfe1d04fabf684d62d537ba
Hao Lin <130253423+pHaoq@users.noreply.github.com>
2023-05-03 10:28:46 +0200

    initial commit

---
+++ src/main/java/com/example/slmcalc/SlmCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmCalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slmcalc/SlmCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmCalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
