b2d59e380e254c3cb68ed930f5253d1eb5a5830b
Hao Lin <130253423+pHaoq@users.noreply.github.com>
2023-05-03 11:04:23 +0200

    add calculator controller and implement sum route

---
+++ src/main/java/com/example/slmcalc/CalculatorController.java
@@ -0,0 +1,21 @@
+package com.example.slmcalc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+
+@RequestMapping("/calc/sum") //beispiel http://localhost:8080/calc/sum?a=5&b=4
+public int sum (
+                @RequestParam int a,
+                @RequestParam int b
+)
+{
+    return a + b;
+}
+
+
+}
